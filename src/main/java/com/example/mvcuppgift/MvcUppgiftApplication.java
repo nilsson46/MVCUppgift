32b67db8a1ef2b14f517be0b027c64822c72aa59
nilsson46 <nilssonsimon46@gmail.com>
2023-01-23 12:46:36 +0100

    Added config

--- src/main/java/com/example/mvcuppgift/MvcUppgiftApplication.java
+++ src/main/java/com/example/mvcuppgift/MvcUppgiftApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MvcUppgiftApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MvcUppgiftApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MvcUppgiftApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/mvcuppgift/MvcUppgiftApplicationTests.java
+++ src/test/java/com/example/mvcuppgift/MvcUppgiftApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MvcUppgiftApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
