63c6ee59c998833acbd92ebf18dd74873b07ca0f
nilsson46 <nilssonsimon46@gmail.com>
2023-01-23 12:43:21 +0100

    Initial commit

---
+++ src/main/java/com/example/mvcuppgift/MvcUppgiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcuppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcUppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcUppgiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvcuppgift/MvcUppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcuppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcUppgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
