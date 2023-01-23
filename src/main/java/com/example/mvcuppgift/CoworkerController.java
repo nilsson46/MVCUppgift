b258ad9477f14fb39abf70e8528c361da75be040
nilsson46 <nilssonsimon46@gmail.com>
2023-01-23 14:28:55 +0100

    Added some classes

---
+++ src/main/java/com/example/mvcuppgift/CoworkerController.java
@@ -0,0 +1,30 @@
+package com.example.mvcuppgift;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CoworkerController {
+
+    @GetMapping("/home")
+
+    public String switchToHomePage(){
+
+        return "home";
+    }
+
+    @PostMapping
+
+    public String switchToInvoicePage(@RequestParam String username, @RequestParam String password, ModelMap model){
+        if("cow1".equalsIgnoreCase(username) && "1234".equals(password)){
+            return "redirect:/invoicePage";
+        } else {
+            model.put("error","Incorrect Username or password");
+            return "home";
+        }
+    }
+}
